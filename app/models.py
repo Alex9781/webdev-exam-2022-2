c61f672f375c733459330ff71f3869966c8d975f
Unidentified Squirrel <alex.zmiev.az@gmail.com>
2022-06-17 14:59:03 +0300

    models and auth

---
+++ app/models.py
@@ -0,0 +1,85 @@
+from flask_login import UserMixin
+from app import db
+import sqlalchemy
+
+
+class User(db.Model, UserMixin):
+    __tablename__ = 'users'
+
+    id = db.Column(db.Integer, primary_key=True)
+    login = db.Column(db.String(100), nullable=False, unique=True)
+    password_hash = db.Column(db.String(256), nullable=False)
+
+    first_name = db.Column(db.String(100), nullable=False)
+    last_name = db.Column(db.String(100), nullable=False)
+    middle_name = db.Column(db.String(100))
+
+    role_id = db.Column(db.Integer, db.ForeignKey('roles.id'), nullable=False)
+    role = db.relationship('Role')
+
+
+class Role(db.Model):
+    __tablename__ = 'roles'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False, unique=True)
+    description = db.Column(db.String(100), nullable=False)
+
+
+class Book(db.Model):
+    __tablename__ = 'books'
+
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False, unique=True)
+
+    short_description = db.Column(db.Text, nullable=False)
+
+    author = db.Column(db.String(100), nullable=False)
+
+    publisher = db.Column(db.String(100), nullable=False)
+    publish_year = db.Column(db.DateTime, nullable=False)
+
+    volume = db.Column(db.Integer, nullable=False)
+
+
+class Genre(db.Model):
+    __tablename__ = 'genres'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False, unique=True)
+
+
+books_genres = db.Table('books_genres',
+    db.Column('book_id', db.Integer, db.ForeignKey('books.id'), nullable=False),
+    db.Column('genre_id', db.Integer, db.ForeignKey('genres.id'), nullable=False)
+)
+
+
+class BookImage(db.Model):
+    __tablename__ = 'books_images'
+
+    id = db.Column(db.String(100), primary_key=True)
+
+    file_name = db.Column(db.String(100), nullable=False)
+    mime_type = db.Column(db.String(100), nullable=False)
+
+    md5_hash = db.Column(db.String(100), nullable=False, unique=True)
+
+    book_id = db.Column(db.Integer, db.ForeignKey('books.id'), nullable=False)
+    book = db.relationship('Book')
+
+
+class Review(db.Model):
+    __tablename__ = 'reviews'
+
+    id = db.Column(db.Integer, primary_key=True)
+    rating = db.Column(db.Integer, nullable=False)
+    text = db.Column(db.Text, nullable=False)
+    
+    created_at = db.Column(db.DateTime, nullable=False, server_default=sqlalchemy.sql.func.now())
+
+    book_id = db.Column(db.Integer, db.ForeignKey('books.id'), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+
+    course = db.relationship('Course')
+    user = db.relationship('User')
